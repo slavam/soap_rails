ce1db4302285821514fd170ba1643eefcf1430a7
slavam <mwm1955@gmail.com>
2024-08-05 10:45:20 +0300

    Check cors for observations

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -1,4 +1,12 @@
 class ObservationsController < ApplicationController
+  before_action :cors_preflight_check
+  # skip_before_action :verify_authenticity_token, :only => [:create_synoptic_telegram]
+  def cors_preflight_check
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
   def observations
     params.to_enum.to_h
 # Rails.logger.debug(">>>>>>>>>>>#{observation_params}<<<<<<<<<")
